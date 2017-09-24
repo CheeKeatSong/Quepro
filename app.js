65784ba40013c122e3dcb363761dff654b115a44
CheeKeatSong <cheekeatsong@gmail.com>
2017-09-24 19:20:05 +0800

    test

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
-var expressValidator = require('express-validator');
-var flash = require('connect-flash');
-var session = require('express-session');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+// var expressValidator = require('express-validator');
+// var flash = require('connect-flash');
+// var session = require('express-session');
+// var passport = require('passport');
+// var LocalStrategy = require('passport-local').Strategy;
 
-// var routes = require('./routes/index');
-// var users = require('./routes/users');
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 // Init App
 var app = express();
