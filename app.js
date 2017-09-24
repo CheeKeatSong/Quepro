e0a9d2ff66399174d3568f762d79cb1e19e2102b
CheeKeatSong <cheekeatsong@gmail.com>
2017-09-24 19:15:40 +0800

    test

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
-// var path = require('path');
+var path = require('path');
 // var cookieParser = require('cookie-parser');
 // var bodyParser = require('body-parser');
-// var exphbs = require('express-handlebars');
+var exphbs = require('express-handlebars');
 // var expressValidator = require('express-validator');
 // var flash = require('connect-flash');
 // var session = require('express-session');
